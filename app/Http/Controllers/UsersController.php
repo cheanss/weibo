7ef8b7bcaa7939df0f28b242f0656285456afa31
Jack.Shi <jack.shi@keerle.net>
2017-01-07 18:01:56 +0800

    user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,15 +7,41 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**
+     * 注册页面显示
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
-        debug(['name'=>'chensuilong']);
         return view('users.create');
     }
 
+    /**
+     * 用户详情页
+     * @param Int $id User ID
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($id)
     {
         $user = User::findOrFail($id);
-        var_dump($user);
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:250',
+            'password' => 'confirmed|required'
+        ]);
+
+//        debug($request->all());
+        $user = User::create([
+           'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return redirect()->route('users.show', [$user]);
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,35 @@
 @extends('layouts.default')
-@section('title', '注册')
+@section('title', '注册账号')
 
 @section('content')
-    <h3>注册页面</h3>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3>注册账号</h3>
+            </div>
+            <div class="panel-body">
+                @include('shared.error')
+                <form action="{{ route('users.store') }}" method="post">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name" class="control-label">名称：</label>
+                        <input type="text" name="name" class="form-control" id="name" value="{{ old('name') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="control-label">邮箱：</label>
+                        <input type="email" name="email" class="form-control" id="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="control-label">密码：</label>
+                        <input type="password" name="password" class="form-control" id="password" value="{{ old('password') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation" class="control-label">确认名称：</label>
+                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" value="{{ old('password_confirmation') }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
